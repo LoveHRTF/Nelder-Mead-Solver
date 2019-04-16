8f86533c5919ba4064a84d9da1a36fbd868ee695
FrozenFace <zwchen95@gmail.com>
2019-04-15 21:58:55 -0400

    Update example.py

--- example.py
+++ example.py
@@ -9,9 +9,10 @@ def f_noise_free(x0):
     x2 = x0[1]
     return (0.065 * x1**4) - (x1**2) + (0.7 * x1) + (0.065 * x2**4) - (x2**2) + (0.9 * x2) + (0.3 * x1 * x2)
 
-
-""" Compute the Noise-Free result"""
-initial_guess = np.array([0, 0, -1, 0, 0, 1]).reshape(3,2)              # Initial Value
-result_vertex = nelder_mead_solve(f_noise_free, initial_guess, 5000)    # Solve for max interations 5000
-result_vertex_centroid = result_vertex.sum(axis=0) / 3                  # Find centroid
-print("Centroid is: ", result_vertex_centroid)                          # Print out centroid
+if __name__ == '__main__':
+    """ Compute the Noise-Free result"""
+    initial_guess = np.array([0, 0, -1, 0, 0, 1]).reshape(3,2)              # Initial Value
+    result_vertex = nelder_mead_solve(f_noise_free, initial_guess, 5000)    # Solve for max interations 5000
+    result_vertex_centroid = result_vertex.sum(axis=0) / 3                  # Find centroid
+    print("Centroid is: ", result_vertex_centroid)                          # Print out centroid
+    
